7f0de75f5c6a5186df76217c99451e2f5f7c63d6
ZakriaJanjua <zakria.janjua@hotmail.com>
2022-07-31 20:41:28 +0500

    feat: completed add client functionality

--- client/src/components/AddClientForm/AddClientForm.jsx
+++ client/src/components/AddClientForm/AddClientForm.jsx
@@ -1,22 +1,78 @@
-import { Fragment } from 'react';
+import { useMutation } from '@apollo/client';
+import { Fragment, useState } from 'react';
+import { ADD_CLIENT } from '../../mutations/clientMutations';
+import { GET_CLIENTS } from '../../queries/clientQueries';
 import './AddClientForm.css';
 
 const AddClientForm = () => {
+	const [formData, setFormData] = useState({ name: '', email: '', phone: '' });
+	const [addClient] = useMutation(ADD_CLIENT);
+
+	const handleChange = (e) => {
+		setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
+
+	const handleClick = (event) => {
+		event.preventDefault();
+		if (
+			formData.name === '' ||
+			formData.email === '' ||
+			formData.phone === ''
+		) {
+			return alert('Please fill all fields');
+		}
+		addClient({
+			variables: {
+				name: formData.name,
+				email: formData.email,
+				phone: formData.phone,
+			},
+			update(cache, { data: { addClient } }) {
+				const { getAllClients } = cache.readQuery({
+					query: GET_CLIENTS,
+				});
+				cache.writeQuery({
+					query: GET_CLIENTS,
+					data: {
+						getAllClients: [...getAllClients, addClient],
+					},
+				});
+			},
+		});
+		setFormData({ name: '', email: '', phone: '' });
+	};
 	return (
 		<Fragment>
-			<button className='button'>Add Client</button>
+			<button className='button' onClick={handleClick}>
+				Add Client
+			</button>
 			<form className='addClientForm'>
 				<div className='label-input'>
 					<label htmlFor='name'>Name</label>
-					<input type='text' name='name'/>
+					<input
+						type='text'
+						name='name'
+						value={formData.name}
+						onChange={handleChange}
+					/>
 				</div>
 				<div className='label-input'>
 					<label htmlFor='email'>Email</label>
-					<input type='text' name='email'/>
+					<input
+						type='email'
+						name='email'
+						value={formData.email}
+						onChange={handleChange}
+					/>
 				</div>
 				<div className='label-input'>
 					<label htmlFor='phone'>Phone</label>
-					<input type='text' name='phone'/>
+					<input
+						type='text'
+						name='phone'
+						value={formData.phone}
+						onChange={handleChange}
+					/>
 				</div>
 			</form>
 		</Fragment>

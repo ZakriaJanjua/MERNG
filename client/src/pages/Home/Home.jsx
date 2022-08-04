273c222ad34e1e2fe60d2606ddfd9066b041d920
ZakriaJanjua <zakria.janjua@hotmail.com>
2022-08-04 23:32:32 +0500

    feat: single project page and API call

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import { Fragment } from 'react';
 import AddClientForm from '../../components/AddClientForm/AddClientForm';
 import Clients from '../../components/Clients/Clients';
-import Header from '../../components/Header/Header';
 import Projects from '../../components/Projects/Projects';
 
 export default function Home() {
 	return (
 		<Fragment>
-			<Header />
 			<AddClientForm />
 			<Clients />
 			<Projects />

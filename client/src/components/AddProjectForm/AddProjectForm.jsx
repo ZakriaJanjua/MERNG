0a0825d35636d0ab852c3a66b0f06664b2806096
ZakriaJanjua <zakria.janjua@hotmail.com>
2022-08-08 00:12:25 +0500

    chore: arranging forms

---
+++ client/src/components/AddProjectForm/AddProjectForm.jsx
@@ -0,0 +1,6 @@
+
+export default function AddProjectForm() {
+  return (
+    <div>AddProjectForm</div>
+  )
+}
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import { Fragment } from 'react';
 import AddClientForm from '../../components/AddClientForm/AddClientForm';
 import Clients from '../../components/Clients/Clients';
 import Projects from '../../components/Projects/Projects';
+import AddProjectForm from '../../components/AddProjectForm/AddProjectForm';
 
 export default function Home() {
 	return (
 		<Fragment>
 			<AddClientForm />
+			<AddProjectForm />
 			<Clients />
 			<Projects />
 		</Fragment>
